d8439c22f0618b14a271401b3059eafc4b913a6c
Oleh Pshyk <pshykol@gmail.com>
2025-11-21 16:35:59 +0200

    style(NGDEV-53): remove uneccesary styles from ProductCard and import styles to AddToFavButton, add color-like and color-background-icon to variables

--- src/components/molecules/AddToFavButton/AddToFavButton.tsx
+++ src/components/molecules/AddToFavButton/AddToFavButton.tsx
@@ -1,11 +1,23 @@
 import Button from '@atoms/Button/Button';
+import { useState } from 'react';
+import './AddToFavButton.scss';
 
 const AddToFavButton = () => {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="fav-button">
+    <div
+      className={`fav-button ${active ? 'is-active' : ''}`}
+      onClick={() => setActive((prev) => !prev)}
+    >
       <Button
         variant="icon"
-        icon={<span className="heart-icon" />}
+        icon={
+          <div className="heart-icon-wrapper">
+            <span className="heart-icon--outline"></span>
+            <span className="heart-icon--filled"></span>
+          </div>
+        }
       />
     </div>
   );
