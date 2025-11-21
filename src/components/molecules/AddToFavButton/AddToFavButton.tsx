df1b319c399ce8f914a7f7e35461900d74910dc2
Bauzi <62158052+Volodymyr-Bauzi@users.noreply.github.com>
2025-11-21 17:13:02 +0200

    Revert "style(NGDEV-53): remove uneccesary styles from ProductCard"

--- src/components/molecules/AddToFavButton/AddToFavButton.tsx
+++ src/components/molecules/AddToFavButton/AddToFavButton.tsx
@@ -1,23 +1,11 @@
 import Button from '@atoms/Button/Button';
-import { useState } from 'react';
-import './AddToFavButton.scss';
 
 const AddToFavButton = () => {
-  const [active, setActive] = useState(false);
-
   return (
-    <div
-      className={`fav-button ${active ? 'is-active' : ''}`}
-      onClick={() => setActive((prev) => !prev)}
-    >
+    <div className="fav-button">
       <Button
         variant="icon"
-        icon={
-          <div className="heart-icon-wrapper">
-            <span className="heart-icon--outline"></span>
-            <span className="heart-icon--filled"></span>
-          </div>
-        }
+        icon={<span className="heart-icon" />}
       />
     </div>
   );
