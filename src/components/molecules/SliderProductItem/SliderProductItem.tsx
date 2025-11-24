3968b557f7addac46813d6ee0ddaceec236003fa
Bauzi <volodiasyd@gmail.com>
2025-11-24 13:07:59 +0200

    refactor(NGDEV-60): removed promise for skeleton

--- src/components/molecules/SliderProductItem/SliderProductItem.tsx
+++ src/components/molecules/SliderProductItem/SliderProductItem.tsx
@@ -1,4 +1,3 @@
-import { useState, type FC } from 'react';
 import './SliderProductItem.scss';
 
 interface SliderProductItemProps {
@@ -6,23 +5,14 @@ interface SliderProductItemProps {
   title: string;
 }
 
-const SliderProductItem: FC<SliderProductItemProps> = ({ image, title }) => {
-  const [loaded, setLoaded] = useState(false);
-
-  const handleLoad = async () => {
-    // Force loading state for 2 seconds to test the skeleton
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    setLoaded(true);
-  };
-
+const SliderProductItem = ({ image, title }: SliderProductItemProps) => {
   return (
     <div className="product__slider__main-img cursor-pointer">
       <div className="slider-product-details__image-wrapper">
         <img
           src={image}
           alt={title}
-          onLoad={handleLoad}
-          className={`max-w-full max-h-full' ${loaded ? 'loaded' : 'hidden'}`}
+          className="max-w-full max-h-full"
         />
       </div>
     </div>
